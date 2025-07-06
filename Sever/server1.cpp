5b376bb2a3550d4d8ec6662047f18fe64b089029
Admin <15802551+sail_3_0@user.noreply.gitee.com>
2025-07-06 19:44:14 +0800

    添加项目文件。

---
+++ Sever/server1.cpp
@@ -0,0 +1,29 @@
+#include "../thirdparty/httplib.h"
+
+#include <string>
+#include <mutex>
+#include <fstream>
+
+std::string str_text;//文本内容
+std::mutex g_mutex;//全局互斥锁
+
+int progress_1 = -1;//玩家进度 
+int progress_2 = -1;
+ 
+void on_hello(const httplib::Request& req, httplib::Response& res) {
+	std::lock_guard<std::mutex> lock(g_mutex);
+
+	std::cout << "Hello From Client" << std::endl;
+
+	res.set_content("Hello From Server", "text/plain");
+}
+
+int main(int argc, char** argy) {
+	httplib::Server server;
+	
+	server.Post("/hello", on_hello);
+
+	server.listen("localhost", 25565);
+
+	return 0;
+}
